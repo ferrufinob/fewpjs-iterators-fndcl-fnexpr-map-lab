75d99898385b93aec121be4230db30ba38d62e4f
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-09 14:21:05 -0500

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials.map((tut) => {
-    let wordArr = tut.split(" ").map((currentValue) => {
+    let wordArr = tut.split(" ").map(function (currentValue) {
       return currentValue[0].toUpperCase() + currentValue.substring(1);
     });
     return wordArr.join(" ");
